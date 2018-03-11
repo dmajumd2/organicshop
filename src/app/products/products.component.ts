1d2cf5d7dc1553f8b0f1b422f23e6b50320d9607
Debashish <dmajumd2@binghamton.edu>
2018-03-11 16:10:05 -0400

    Added code for filtering the categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from './../category.service';
 
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -10,14 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent {
 
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: String;
 
-  constructor(productService: ProductService, categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService) {
+
+    productService.getAll().subscribe(products => this.products = products);
     this.categories$ =  categoryService.getAll();
-   }
 
-  
+    route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+        this.filteredProducts = (this.category) ?
+            this.products.filter(p => p.category === this.category) :
+            this.products;
+    });
+  } 
 
 }
