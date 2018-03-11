7ed0cb9f4f8cb1c0eb1263199bcfb761e8ea470b
Debashish <dmajumd2@binghamton.edu>
2018-03-11 13:15:14 -0400

    commit for the ecommerce app

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCayN2xGLjj0dkaat1UDEkiSPOG2mZJ_0E",
+    authDomain: "oshop-93fce.firebaseapp.com",
+    databaseURL: "https://oshop-93fce.firebaseio.com",
+    projectId: "oshop-93fce",
+    storageBucket: "oshop-93fce.appspot.com",
+    messagingSenderId: "116871041127"    
+  }
+};
