54515e2de0d2a4f81c88a633b21e3d2db0d31546
Debashish <dmajumd2@binghamton.edu>
2018-03-17 13:28:14 -0400

    Added router to place order to order success page

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,8 +1,11 @@
+import { AuthService } from './../auth.service';
 import { OrderService } from './../order.service';
 import { ShoppingCartService } from './../shopping-cart.service';
 import { ShoppingCart } from './../models/shopping-cart';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import { Order } from '../models/order';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check-out',
@@ -13,38 +16,32 @@ import { Subscription } from 'rxjs/Subscription';
 export class CheckOutComponent implements OnInit, OnDestroy{ 
   shipping = {}; 
   cart: ShoppingCart;
-  subscription: Subscription;
+  cartSubscription: Subscription;
+  userId: string;
+  userSubscription: Subscription;
 
   constructor(
+    private router: Router,
     private orderService: OrderService,
-    private shoppingCartService: ShoppingCartService) {}
+    private shoppingCartService: ShoppingCartService,
+    private authService: AuthService
+  ) {}
 
   async ngOnInit(){
     let cart$ =  await this.shoppingCartService.getCart();
-    this.subscription = cart$.subscribe(cart => this.cart = cart);
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
   }
 
   ngOnDestroy(){
-    this.subscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
   
   //placing data to db from checkout page via order service 
-  placeOrder() {
-    let order = {
-      datePlaced: new Date().getTime(),
-      shipping: this.shipping,
-      items: this.cart.items.map(i => {
-        return {
-          product: {
-            title: i.title,
-            imageUrl: i.imageUrl,
-            price: i.price
-          },
-          quantity: i.quantity,
-          totalPrice: i.totalPrice
-        }
-      })
-    };
-    this.orderService.storeOrder(order);
+  async placeOrder() {
+    let order = new Order(this.userId, this.shipping, this.cart); // order.ts model
+    let result = await this.orderService.storeOrder(order);
+    this.router.navigate(['/order-success', result.key]);
   }    
 }
